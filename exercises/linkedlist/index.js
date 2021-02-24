1ebf8ef26ea5f72fbf317a76cfa28f4aa054dd59
SonPhan <phantruongson96@gmail.com>
2021-02-23 22:59:30 -0500

    for each

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -135,6 +135,16 @@ class LinkedList {
 
     previous.next = node;
   }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+    while(node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
