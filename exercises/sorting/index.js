691d8210326edce1d49750507b13e8e65fc72a91
SonPhan <phantruongson96@gmail.com>
2021-02-25 23:31:50 -0500

    selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -15,7 +15,21 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for(let i =0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for(let j = i+1; j < arr.length; j++){
+       if(arr[j] < arr[indexOfMin]){
+         indexOfMin = j;
+       }
+    }
 
+    if(indexOfMin !== i) {
+      const temp = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
